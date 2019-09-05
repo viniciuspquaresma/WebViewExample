8fbe2a4fdfd0a6c0949bb11294772cab971a3005
Vinicius Quaresma <vinicius.pereira@mercadolivre.com>
2019-09-04 23:29:46 -0300

    Update ViewController.swift
    Alterando o link para https://www.mercadopago.com.br/checkout/v1/redirect?pref_id=438050834-36b33107-3f50-4678-ae51-d5b1eda5c96e

--- WebViewExample/ViewController.swift
+++ WebViewExample/ViewController.swift
@@ -20,15 +20,15 @@ class ViewController: UIViewController,WKNavigationDelegate {
     override func viewDidLoad() {
         super.viewDidLoad()
 //        load remote url
-//        let url = URL(string: "https://www.google.com")!
-//        webView.load(URLRequest(url: url))
-//        webView.allowsBackForwardNavigationGestures = true
+        let url = URL(string: "https://www.mercadopago.com.br/checkout/v1/redirect?pref_id=438050834-36b33107-3f50-4678-ae51-d5b1eda5c96e")!
+        webView.load(URLRequest(url: url))
+        webView.allowsBackForwardNavigationGestures = true
         
 //        load local html file
-        let url = Bundle.main.url(forResource: "example", withExtension: "html", subdirectory: "local_html")!
-        webView.loadFileURL(url, allowingReadAccessTo: url)
-        let request = URLRequest(url: url)
-        webView.load(request)
+//        let url = Bundle.main.url(forResource: "example", withExtension: "html", subdirectory: "local_html")!
+//        webView.loadFileURL(url, allowingReadAccessTo: url)
+//        let request = URLRequest(url: url)
+//        webView.load(request)
     }
     
     override func loadView() {
